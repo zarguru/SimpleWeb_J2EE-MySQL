621c789f7f7cc8d52689a1a554438ff7547fa6ba
zarguru <zarguru@gmail.com>
2017-08-05 22:41:40 -0400

    Add files via upload

---
+++ src/edu/Hello.java
@@ -0,0 +1,13 @@
+package edu;
+
+public class Hello {
+	
+	public String sayHello(String name) {
+		return "Hello " + name;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
